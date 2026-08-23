df6d4a61ebfda814d29a0b0e51c6d4a4151d525c
4Denthusiast <25589515+4Denthusiast@users.noreply.github.com>
2026-08-23 18:02:44 +0100

    Make the projected renderer's background opaque black.

--- render/3d/combined/combined_rendering_engine_4d.cpp
+++ render/3d/combined/combined_rendering_engine_4d.cpp
@@ -4,6 +4,7 @@
 #include "depth_capture.glsl.gen.h"
 
 #if GDEXTENSION
+#include <godot_cpp/classes/canvas_item_material.hpp>
 #include <godot_cpp/classes/rd_sampler_state.hpp>
 #include <godot_cpp/classes/rd_shader_source.hpp>
 #include <godot_cpp/classes/rd_shader_spirv.hpp>
@@ -17,6 +18,7 @@
 #include <godot_cpp/classes/viewport_texture.hpp>
 #include <godot_cpp/variant/packed_byte_array.hpp>
 #elif GODOT_MODULE
+#include "scene/resources/canvas_item_material.h"
 #include "servers/rendering/renderer_rd/storage_rd/render_scene_buffers_rd.h"
 #include "servers/rendering/renderer_rd/uniform_set_cache_rd.h"
 #include "servers/rendering/rendering_device.h"
@@ -106,6 +108,10 @@ void CombinedRenderingEngine4D::_ensure_helpers_created() {
 		_projected_rect->set_texture(_projected_viewport->get_texture());
 		_projected_rect->set_anchors_preset(Control::PRESET_FULL_RECT);
 		_projected_rect->set_mouse_filter(Control::MOUSE_FILTER_IGNORE);
+		Ref<CanvasItemMaterial> projected_rect_material;
+		projected_rect_material.instantiate();
+		projected_rect_material->set_blend_mode(CanvasItemMaterial::BLEND_MODE_PREMULT_ALPHA);
+		_projected_rect->set_material(projected_rect_material);
 		_combine_canvas_layer->add_child(_projected_rect);
 	}
 }
--- render/3d/projected/projected_rendering_engine_4d.cpp
+++ render/3d/projected/projected_rendering_engine_4d.cpp
@@ -304,7 +304,8 @@ void ProjectedRenderingEngine4D::_normalize_image_callback(int64_t p_effect_call
 			push_constant_data[1] = size.y;
 			float *push_constant_float_data = reinterpret_cast<float *>(push_constant.ptrw());
 			push_constant_float_data[2] = (float)camera->get_projection_opacity_base();
-			push_constant_data[3] = 0;
+			// 3 is transparency. `_cross_section_depth_texture` is set iff this is running as part of the combined renderer.
+			push_constant_data[3] = _cross_section_depth_texture.get_type() == Variant::NIL ? 0 : 1;
 		}
 		for (uint32_t view = 0; view < buffers->get_view_count(); view++) {
 			Ref<RDUniform> uniform;
